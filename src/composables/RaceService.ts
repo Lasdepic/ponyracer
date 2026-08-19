731fd91a2073eaa393098f659c958497e2671772
Lasdepic <clementjorda34@gmail.com>
2026-08-19 09:36:31 +0200

    appel API avec axios sans try catch

--- src/composables/RaceService.ts
+++ src/composables/RaceService.ts
@@ -1,36 +1,15 @@
-import { ref } from 'vue'
+import axios from 'axios'
 import type { RaceModel } from '../models/RaceModel'
 
 export function useRaceService() {
   return {
-    list(): Promise<Array<RaceModel>> {
-      const races = ref<Array<RaceModel>>([
-        {
-          id: 12,
-          name: 'Paris',
-          ponies: [
-            { id: 1, name: 'Gentle Pie', color: 'YELLOW' },
-            { id: 2, name: 'Big Soda', color: 'ORANGE' },
-            { id: 3, name: 'Gentle Bottle', color: 'PURPLE' },
-            { id: 4, name: 'Superb Whiskey', color: 'GREEN' },
-            { id: 5, name: 'Fast Rainbow', color: 'BLUE' },
-          ],
-          startInstant: '2020-02-18T08:02:00Z',
+    async list(): Promise<Array<RaceModel>> {
+      const res = await axios.get<Array<RaceModel>>('https://ponyracer.ninja-squad.com/api/races', {
+        params: {
+          status: 'PENDING',
         },
-        {
-          id: 13,
-          name: 'Tokyo',
-          ponies: [
-            { id: 6, name: 'Fast Rainbow', color: 'BLUE' },
-            { id: 7, name: 'Gentle Castle', color: 'GREEN' },
-            { id: 8, name: 'Awesome Rock', color: 'PURPLE' },
-            { id: 9, name: 'Little Rainbow', color: 'YELLOW' },
-            { id: 10, name: 'Great Soda', color: 'ORANGE' },
-          ],
-          startInstant: '2020-02-18T08:03:00Z',
-        },
-      ])
-      return Promise.resolve(races.value);
+      })
+      return res.data
     },
   }
 }
