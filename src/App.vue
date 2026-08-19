9c086cc0428feb85b4694ccb908fd53bb23d3fd7
Lasdepic <clementjorda34@gmail.com>
2026-08-19 11:42:10 +0200

    Encapsuler les erreurs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onErrorCaptured } from 'vue'
+import Alert from './components/Alert.vue'
+
+const error = ref(false)
+
+onErrorCaptured(() => {
+  error.value = true
+  return false
+})
+</script>
 
 <template>
   <Navbar />
   <main class="container" style="margin-top: 50px">
     <h1 class="text-center fw-bold text-dark mb-4 p-2">Ponyracer</h1>
-    <Races />
+    <Alert v-if="error" variant="danger" :dismissible="true" @dismissed="error = false">
+      An error occurred while loading.
+    </Alert>
+    <Suspense>
+      <template #default>
+        <Races />
+      </template>
+      <template #fallback>
+        <div role="status">Loading.</div>
+      </template>
+    </Suspense>
   </main>
 </template>
 
@@ -12,6 +32,7 @@
 import { defineComponent } from 'vue'
 import Navbar from './components/Navbar.vue'
 import Races from './pages/Races.vue'
+import Alert from './components/Alert.vue'
 
 export default defineComponent({
   name: 'App',
@@ -19,6 +40,7 @@ export default defineComponent({
   components: {
     Navbar,
     Races,
+    Alert,
   },
 })
 </script>
--- src/pages/Races.vue
+++ src/pages/Races.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
 import type { RaceModel } from '../models/RaceModel'
 import Race from '../components/Race.vue'
 import { useRaceService } from '../composables/RaceService'
 
-const races = ref<Array<RaceModel>>([])
-
 const raceService = useRaceService()
 
-onMounted(async () => {
-  try {
-    races.value = await raceService.list()
-  } catch {
-    error.value = true
-  }
-})
+const races: Array<RaceModel> = await raceService.list()
 </script>
 
 <template>
@@ -22,10 +13,6 @@ onMounted(async () => {
     <h1 class="mb-4 border border-dark text-center text-warning bg-dark rounded">
       Course de Poney
     </h1>
-
-    <Alert v-if="error" variant="danger" dismissible @dismissed="error = false">
-      An error occurred while loading.
-    </Alert>
 
     <Race v-for="race in races" :key="race.id" :raceModel="race" />
   </div>
