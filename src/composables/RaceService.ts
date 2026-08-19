a7879f4fb695b0816a950908cace8cc86ff9b71e
Lasdepic <clementjorda34@gmail.com>
2026-08-19 09:48:55 +0200

    Gestion d'erreur appel API

--- src/composables/RaceService.ts
+++ src/composables/RaceService.ts
@@ -4,12 +4,21 @@ import type { RaceModel } from '../models/RaceModel'
 export function useRaceService() {
   return {
     async list(): Promise<Array<RaceModel>> {
-      const res = await axios.get<Array<RaceModel>>('https://ponyracer.ninja-squad.com/api/races', {
-        params: {
-          status: 'PENDING',
-        },
-      })
-      return res.data
+      try {
+        const res = await axios.get<Array<RaceModel>>(
+          'https://ponyracer.ninja-squad.com/api/races',
+          {
+            params: {
+              status: 'PENDING',
+            },
+          },
+        )
+        return res.data
+      } catch (error) {
+        console.error('Erreur lors de la récupération des courses :', error)
+
+        throw error
+      }
     },
   }
 }
